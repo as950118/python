a40f8575cbccc978bcf365a2fba73345d99918bd
as950118 <na_qa@icloud.com>
2018-06-14 13:35:10 +0900

    Python

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,7 +83,7 @@ DATABASES = {
         'USER' : 'root',
         'PASSWORD' : 'password',
         'HOST' : 'localhost',
-        'PORT' : ''
+        'PORT' : '3306'
     }
 }
 
