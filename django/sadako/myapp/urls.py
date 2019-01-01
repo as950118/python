5cb0839e0bdf44baa582e2fec5695fa4b13bb317
as950118 <na_qa@icloud.com>
2019-01-01 19:35:04 +0900

    Algorithm

--- django/sadako/myapp/urls.py
+++ django/sadako/myapp/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = {
     url(r'^$', views.main, name = 'main'),
-    url(r'signup/', views.board_1, name = 'signup'),
-    url(r'login/', views.board_1, name = 'login'),
-    url(r'mypage/', views.board_1, name = 'myapp'),
-    url(r'bbs/', views.board_1, name = 'bbs'),
-    url(r'gallery/', views.board_1, name = 'gallery'),
+    url(r'signup/', views.signup, name = 'signup'),
+    url(r'login/', views.login, name = 'login'),
+    url(r'mypage/', views.mypage, name = 'myapp'),
+    url(r'bbs/', views.bbs, name = 'bbs'),
+    url(r'gallery/', views.gallery, name = 'gallery'),
+    url(r'signup_post/', views.signup_post, name = 'signup_post'),
 }
--- django/sadako/myapp/views.py
+++ django/sadako/myapp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def main(req):
     return render(req, 'myapp/main.html',{})
+def signup(req):
+    return render(req, 'myapp/signup.html',{})
+def login(req):
+    return render(req, 'myapp/login.html',{})
+def mypage(req):
+    return render(req, 'myapp/mypage.html',{})
+def bbs(req):
+    return render(req, 'myapp/bbs.html',{})
+def gallery(req):
+    return render(req, 'myapp/gallery.html',{})
+
+def signup_post(req):
+    id = req.POST['id']
+    print(id)
+    return render(req, 'myapp/main.html',{})
