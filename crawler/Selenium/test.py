1b2af0af3e44f2f7d44cea23feaa4a0224779213
as950118 <na_qa@icloud.com>
2019-01-06 19:39:38 +0900

    Selenium

---
+++ crawler/Selenium/test.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+wd = webdriver.Chrom() #같은 폴더에 있으므로 생략, 만약 다른 디렉터리에 있다면 명시해줘야함
+driver.get("https://www.naver.com/") #네이버에 접속
