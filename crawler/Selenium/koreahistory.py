455fad838ab3e6e12b5cb49d7e128051a559c6db
as950118 <na_qa@icloud.com>
2019-02-06 18:20:00 +0900

    Selenium

--- crawler/Selenium/koreahistory.py
+++ crawler/Selenium/koreahistory.py
@@ -1,17 +1,36 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
 wd = webdriver.Chrome()
 wd.get("http://www.historyexam.go.kr/pst/list.do?bbs=dat")
-elem = wd.find_elements_by_xpath("//*[@class='page3']")
-#print(elem)
-#elem = wd.find_element_by_partial_link_text("pageIndex")
-print(elem[0].text)
-for link in elem:
-    try:
-        print(link.text)
-        link.click()
-    except:
-        #print("error"+link.text)
-        pass
+M = 1 #페이지 순서
+while 1:
+    N = 1 #게시글 순서
+    print("next tab")
+    while 1:
+        temp = 3*(N)-(((M%7)-1)%3)
+        print(N,M,3*(N)-(((M%7)-1)%3))
+        if 3*(N)-(((M%7)-1)%3)>10:
+            print("next page")
+            break
+        elem = wd.find_elements_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/table/tbody/tr[' + str(temp) + ']/td[2]/a')
+
+        print(elem)
+        print(elem[0].text)
+        elem[0].click()
+        #wd.find_element_by_partial_link_text("문제지").click()
+        #wd.find_element_by_partial_link_text("정답표").click()
+        wd.execute_script("window.history.go(-1)")
+        N+=1
+    M+=1
+    wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li['+str(M%5 + 3)+']/a').click()
+
+'''
+elem = wd.find_element_by_partial_link_text("고급")
+elem.click()
+wd.execute_script("window.history.go(-1)")
+'''
+'''
+for i in range(2,6):
+    elem = wd.find_element_by_link_text(str(i))
+    elem.click()
+'''
--- crawler/Selenium/test.py
+++ crawler/Selenium/test.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 wd = webdriver.Chrome() #같은 폴더에 있으므로 생략, 만약 다른 디렉터리에 있다면 명시해줘야함
-wd.get("https://www.naver.com/") #네이버에 접속
+#wd.get('https://naver.com') #네이버에 접속
+print(wd.current_url)
+
 elem = wd.find_element_by_id('query')
 elem.send_keys('헌진정')
 elem.submit()
