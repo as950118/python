01723898e4c3d32b009c8ac64beea5fc82be6a0e
as950118 <na_qa@icloud.com>
2019-02-07 19:45:48 +0900

    Selenium

--- crawler/Selenium/koreahistory.py
+++ crawler/Selenium/koreahistory.py
@@ -2,35 +2,57 @@ from selenium import webdriver
 
 wd = webdriver.Chrome()
 wd.get("http://www.historyexam.go.kr/pst/list.do?bbs=dat")
-M = 1 #페이지 순서
+
+def movetoboard():
+    global index
+    elems = wd.find_elements_by_xpath("//a[@href]")
+    for x in elems[index+1:]:
+        if '고급' in x.text or '1급' in x.text or '2급' in x.text:
+            print(x.text) #게시글 제목 출력
+            index = elems.index(x) #인덱스 수정
+            x.click()
+            '''
+            try: #문제지와 정답표가 따로있는 경우도 있으므로
+                wd.find_element_by_partial_link_text("문제지").click()
+            except:
+                continue
+            try:
+                wd.find_element_by_partial_link_text("정답표").click()
+            except:
+                continue
+            '''
+            wd.find_elements_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/div/div/a')[0].click()
+            return 1
+    return 0
+
+
+
+M = 1 #페이지
 while 1:
-    N = 1 #게시글 순서
-    print("next tab")
-    while 1:
-        temp = 3*(N)-(((M%7)-1)%3)
-        print(N,M,3*(N)-(((M%7)-1)%3))
-        if 3*(N)-(((M%7)-1)%3)>10:
-            print("next page")
-            break
-        elem = wd.find_elements_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/table/tbody/tr[' + str(temp) + ']/td[2]/a')
+    index = -1 #초기값
+    while movetoboard():
+        continue
 
-        print(elem)
-        print(elem[0].text)
-        elem[0].click()
-        #wd.find_element_by_partial_link_text("문제지").click()
-        #wd.find_element_by_partial_link_text("정답표").click()
-        wd.execute_script("window.history.go(-1)")
-        N+=1
     M+=1
-    wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li['+str(M%5 + 3)+']/a').click()
+    if M%6==0:
+        wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li[9]/a').click()
+    else:
+        wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li['+str((M-1)%5 + 4)+']/a').click()
 
 '''
-elem = wd.find_element_by_partial_link_text("고급")
-elem.click()
-wd.execute_script("window.history.go(-1)")
-'''
-'''
-for i in range(2,6):
-    elem = wd.find_element_by_link_text(str(i))
-    elem.click()
+def back(PageNum):
+    wd.get("http://www.historyexam.go.kr/pst/list.do?bbs=dat")
+    while PageNum>5:
+        wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li[9]/a').click()
+        PageNum -= 5
+    return PageNum
+
+def movepage(PageNum):
+    while PageNum>5:
+        wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li[9]/a').click()
+        PageNum -= 5
+    if PageNum == 1:
+        return PageNum
+    wd.find_element_by_xpath('//*[@id="sub_content"]/div[2]/div[3]/ul/li['+str(PageNum+3) +']/a').click()
+    return PageNum
 '''
